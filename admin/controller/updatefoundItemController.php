93af4c52de63888a8a440f7e6d270e26c897a725
azeem <azeemafridi676@gmail.com>
2024-08-04 16:24:53 +0500

    lost done found done

--- admin/controller/updatefoundItemController.php
+++ admin/controller/updatefoundItemController.php
@@ -1,44 +1,29 @@
 <?php
     include_once "../config/dbconnect.php";
 
-    $fitemId=$_POST['fitemId'];
-    $fitemName= $_POST['fitemName'];
-    $fItemDescription= $_POST['fItemDescription'];
-    $Contact= $_POST['Contact'];
-    $ficategory= $_POST['ficategory'];
+    $fitemId = $_POST['fitemId'];
+    $fitemName = $_POST['fitemName'];
+    $category_name = $_POST['category_name'];
+    $FDate = $_POST['FDate'];
+    $fLocation = $_POST['fLocation'];
+    $fTime = $_POST['fTime'];
+    $fItemDescription = $_POST['fItemDescription'];
+    $Contact = $_POST['Contact'];
+    
 
-    if( isset($_FILES['newImage']) ){
-        
-        $location="./uploads/";
-        $img = $_FILES['newImage']['name'];
-        $tmp = $_FILES['newImage']['tmp_name'];
-        $dir = '../uploads/';
-        $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif','webp');
-        $image =rand(1000,1000000).".".$ext;
-        $final_image=$location. $image;
-        if (in_array($ext, $valid_extensions)) {
-            $path = UPLOAD_PATH . $image;
-            move_uploaded_file($tmp, $dir.$image);
-        }
-    }else{
-        $final_image=$_POST['existingImage'];
-    }
-    $updateItem = mysqli_query($conn,"UPDATE reportfound SET 
-        fitemName='$litemName', 
-        fItemDescription='$lItemDescription', 
-        Contact=$Contact,
-        categoryId=$category,
-        itemimage='$final_image' 
+    $updateItem = mysqli_query($conn, "UPDATE reportfound SET 
+        fitemName='$fitemName', 
+        category_name='$category_name', 
+        FDate='$FDate', 
+        fLocation='$fLocation',     
+        fTime='$fTime', 
+        fItemDescription='$fItemDescription', 
+        Contact='$Contact'
         WHERE fitemId=$fitemId");
 
-
-    if($updateItem)
-    {
+    if ($updateItem) {
         echo "true";
+    } else {
+        echo mysqli_error($conn);
     }
-    // else
-    // {
-    //     echo mysqli_error($conn);
-    // }
 ?>
